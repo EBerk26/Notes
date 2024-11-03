6b296a51b2443171f8d96fca016992f8866ad071
EBerk26 <eli_berk26@milton.edu>
2024-11-03 15:02:09 -0500

    Finished the 3x3 exhaustive case.

--- src/MrHalesOct31.java
+++ src/MrHalesOct31.java
@@ -1,6 +1,8 @@
 public class MrHalesOct31 {
 
     public int[][] traffic;
+    int bestCost;
+    String bestRoute;
 
 
     public static void main(String[] args) {
@@ -8,6 +10,7 @@ public class MrHalesOct31 {
     }
     public MrHalesOct31() {
         traffic=new int[3][3];
+        bestCost = 99*traffic.length; //the highest possible cost of just going diagonal
         for(int x =0;x< traffic.length;x++){
             for(int y =0;y< traffic[0].length;y++){
                 traffic[x][y] =(int)(100*Math.random());
@@ -17,22 +20,26 @@ public class MrHalesOct31 {
         }
         System.out.println();
         move(0, 0, 0,"");
+        System.out.println(bestRoute);
     }
     public void move(int x, int y , int accruedCost, String pathString){
         accruedCost+=traffic[y][x];
-        if(y== traffic.length-1&&x== traffic[0].length-1){
-            System.out.println("Done. Cost = "+accruedCost+". Path was:"+pathString.substring(0,pathString.length()-1)+".");
-        } else {
-            boolean spaceToGoDown = y<traffic.length-1;
-            boolean spaceToGoRight = x<traffic[0].length-1;
-            if(spaceToGoRight){
-                move(x+1, y, accruedCost,pathString+" right,");
-            }
-            if(spaceToGoDown){
-                move(x, y+1, accruedCost,pathString+" down,");
-            }
-            if(spaceToGoRight&&spaceToGoDown) {
-                move(x + 1, y + 1, accruedCost,pathString+" diagonal,");
+        if(accruedCost<bestCost) { //if your cost at any point can be beaten, don't continue down that path.
+            if (y == traffic.length - 1 && x == traffic[0].length - 1) {
+                    bestCost = accruedCost;
+                    bestRoute="Done. Cost = " + accruedCost + ". Path was:" + pathString.substring(0, pathString.length() - 1) + ".";
+            }else {
+                boolean spaceToGoDown = y < traffic.length - 1;
+                boolean spaceToGoRight = x < traffic[0].length - 1;
+                if (spaceToGoRight && spaceToGoDown) {
+                    move(x + 1, y + 1, accruedCost, pathString + " diagonal,");
+                }
+                if (spaceToGoRight) {
+                    move(x + 1, y, accruedCost, pathString + " right,");
+                }
+                if (spaceToGoDown) {
+                    move(x, y + 1, accruedCost, pathString + " down,");
+                }
             }
         }
     }
