54420181789b236520446673d5d9950f651c55b7
EBerk26 <eli_berk26@milton.edu>
2024-11-07 17:52:06 -0500

    Finished A*. Next up is to create something that compares it with my exhaustive search.

--- src/AStarTryTwo.java
+++ src/AStarTryTwo.java
@@ -1,12 +1,14 @@
 public class AStarTryTwo {
     public int[][] traffic;
-    int size =5;
+    int size =50;
     int startX = 0;
     int startY=0;
     int endX=size-1;
     int endY=size-1;
     Path[] openList;
     Path[] closedList;
+    long startTime;
+    int distanceCoefficient = 50; //50 - A* 0 - Dijkstra's
     public static void main(String[] args) {
         new AStarTryTwo();
     }
@@ -28,9 +30,11 @@ public class AStarTryTwo {
             }
             System.out.println();
         }
+        System.out.println();
         openList=new Path[1];
-        closedList = new Path[1];
+        closedList = new Path[0];
         openList[0]=new Path("",traffic[startX][startY],traffic[startX][startY],startX,startY);
+        startTime = System.currentTimeMillis();
         while(openList.length!=0){
             int bestF = 100*distance(startX,startY);
             int bestIndex = 0;
@@ -42,7 +46,145 @@ public class AStarTryTwo {
             }
             Path q = openList[bestIndex];
             removeFromOpenList(bestIndex);
+            Path[] successor = new Path[9];
+            for(int x =1;x<=8;x++){
+                successor[x]=new Path();
+                successor[x].setParent(q,q.x,q.y);
+                if(x==1){
+                    successor[x].x=q.x;
+                    successor[x].y=q.y-1;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0){
+                        successor[x].g=q.g+traffic[successor[x].x][successor[x].y];
+                        successor[x].h=distanceCoefficient*distance(successor[x].x,successor[x].y);
+                        successor[x].f=successor[x].g+successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if(!betterSolutionExistsForSamePlace){
+                            addToList(successor[x],"open");
+                        }
+                    }
+                } else if (x==2){
+                    successor[x].x=q.x+1;
+                    successor[x].y=q.y-1;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                } else if (x==3){
+                    successor[x].x=q.x+1;
+                    successor[x].y=q.y;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                } else if (x==4){
+                    successor[x].x=q.x+1;
+                    successor[x].y=q.y+1;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                } else if (x==5){
+                    successor[x].x=q.x;
+                    successor[x].y=q.y+1;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                } else if(x==6){
+                    successor[x].x=q.x-1;
+                    successor[x].y=q.y+1;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                } else if (x==7){
+                    successor[x].x=q.x-1;
+                    successor[x].y=q.y;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                } else{
+                    successor[x].x=q.x-1;
+                    successor[x].y=q.y-1;
+                    if(successor[x].x==endX&&successor[x].y==endY){
+                        printOutPath(successor[x]);
+                        break;
+                    } else if (successor[x].x<size&&successor[x].x>0&&successor[x].y<size&&successor[x].y>0) {
+                        successor[x].g = q.g + traffic[successor[x].x][successor[x].y];
+                        successor[x].h = distanceCoefficient * distance(successor[x].x, successor[x].y);
+                        successor[x].f = successor[x].g + successor[x].h;
+                        boolean betterSolutionExistsForSamePlace = isBetterSolutionExistsForSamePlace(successor, x);
+                        if (!betterSolutionExistsForSamePlace) {
+                            addToList(successor[x], "open");
+                        }
+                    }
+                }
+            }
+            addToList(q,"closed");
         }
+    }
+
+    private boolean isBetterSolutionExistsForSamePlace(Path[] successor, int x) {
+        boolean betterSolutionExistsForSamePlace = false;
+        for (int y = 0; y < openList.length; y++) {
+            if (((openList.length!=0&&(openList[y].x == successor[x].x && openList[y].y == successor[x].y && openList[y].f < successor[x].f)) || (y<closedList.length && (closedList[y].x == successor[x].x && closedList[y].y == successor[x].y && closedList[y].f < successor[x].f)))) {
+                betterSolutionExistsForSamePlace = true;
+                break;
+            }
+        }
+        return betterSolutionExistsForSamePlace;
     }
 
     int distance(int x, int y){
@@ -71,6 +213,48 @@ public class AStarTryTwo {
         openList=new Path[temp.length];
         System.arraycopy(temp,0,openList,0,temp.length);
     }
+    void printOutPath(Path input){
+        openList=new Path[0];
+        System.out.println("Cost: "+((input.parent.g+traffic[input.x][input.y])));
+        int[][] traversedSquares;
+        traversedSquares = new int[input.generation+1][2];
+        Path backtracking = input;
+        int i =0;
+        while(backtracking.generation>0){
+            traversedSquares[i][0]=backtracking.x;
+            traversedSquares[i][1]=backtracking.y;
+            backtracking=backtracking.parent;
+            i++;
+        }
+        traversedSquares[i][0]=backtracking.x;
+        traversedSquares[i][1]=backtracking.y;
+        for(int y =0;y< traffic[0].length;y++){
+            for(int x =0;x< traffic.length;x++){
+                if(x==startX&&y==startY){
+                    System.out.print(TextColors.GREEN+traffic[x][y]+TextColors.RESET+" ");
+                } else if(x==endX&&y==endY){
+                    System.out.print(TextColors.RED+traffic[x][y]+TextColors.RESET+" ");
+                } else{
+                    boolean printedOutAYellowNumber = false;
+                    for(int z =0;z<traversedSquares.length;z++){
+                        if(x==traversedSquares[z][0]&&y==traversedSquares[z][1]){
+                            System.out.print(TextColors.YELLOW+traffic[x][y] + " "+TextColors.RESET);
+                            printedOutAYellowNumber=true;
+                        }
+                    }
+                    if(!printedOutAYellowNumber) {
+                        System.out.print(traffic[x][y] + " ");
+                    }
+                }
+                if(traffic[x][y]<10){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("A* Solution took "+(System.currentTimeMillis()-startTime)+" milliseconds.");
+    }
+
 
 }
 
--- src/Path.java
+++ src/Path.java
@@ -3,9 +3,13 @@ public class Path {
     int g;
     int f;
     int x;
+    int h;
     int y;
     int parentX;
     int parentY;
+    Path parent;
+    boolean hasParent = false;
+    int generation=0;
     public Path(String howToGetThere,int g,int f,int x,int y) {
         this.howToGetThere=howToGetThere;
         this.g=g;
@@ -16,8 +20,14 @@ public class Path {
     public Path(int x){
         this.x=x;
     }
-    void setParent(int parentX, int parentY){
+    public Path(){
+
+    }
+    void setParent(Path parent,int parentX, int parentY){
+        this.parent=parent;
         this.parentX=parentX;
         this.parentY=parentY;
+        this.hasParent = true;
+        generation = parent.generation+1;
     }
 }
